affe9ea7c09af95dc2b7faab98557090f2484212
githubsamuell <samuel.guilherme.l@hotmail.com>
2024-07-31 17:44:27 -0300

    arrumando enderço do local host para testar no ganache

--- src/Web3.js
+++ src/Web3.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3';
-import DecentralizedLending from '../build/contracts/DecentralizedLending.json';
+import DecentralizedLending from './DecentralizedLending.json';
 
-const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
 const contractAddress = "0x5b29D567f6FC9c18692f8c9745b6aADa16a6Ed00"; // futuramente ira funcionar por input do usuario..
 const contract = new web3.eth.Contract(DecentralizedLending.abi, contractAddress);
 
